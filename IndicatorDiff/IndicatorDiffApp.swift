b27408ee79cc99523522415b1a8fa4412e779d17
gzonelee <gzonelee@gmail.com>
2026-04-21 18:27:21 +0900

    Initial Commit

---
+++ IndicatorDiff/IndicatorDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndicatorDiffApp.swift
+//  IndicatorDiff
+//
+//  Created by gzonelee on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct IndicatorDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
